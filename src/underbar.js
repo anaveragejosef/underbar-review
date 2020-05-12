5ae55cd43665815cdfa6225c2f0ad216b0ea4f74
Kim Dang <kimdang229@gmail.com>
2020-05-12 11:10:50 -0700

    Began implementation of part1 underbar functions

--- src/underbar.js
+++ src/underbar.js
@@ -47,12 +47,12 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
-    if (Array.isArray(collection)){
-      for (var i = 0; i < collection.length; i++){
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
         iterator(collection[i], i, collection);
       }
     } else {
-      for(var key in collection){
+      for (var key in collection) {
         iterator(collection[key], key, collection);
       }
     }
@@ -78,8 +78,8 @@
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
     var output = [];
-    _.each(collection, function(item){
-      if(test(item) === true){
+    _.each(collection, function(item) {
+      if (test(item) === true) {
         output.push(item);
       }
     });
@@ -91,7 +91,7 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    return _.filter(collection, function(item){
+    return _.filter(collection, function(item) {
       return test(item) === false;
     });
   };
