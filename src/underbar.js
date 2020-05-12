5b28f281cc65b2ab3d420026337c806323e800f5
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-12 15:47:43 -0700

    Add Logic to Allow for .reduce to Pass Tests
    This commit adds the logic that allows for .reduce to pass all tests in
    part1.js

--- src/underbar.js
+++ src/underbar.js
@@ -169,6 +169,16 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var hasNoAccumulator = arguments.length === 2;
+    _.each(collection, function(element) {
+      if (hasNoAccumulator === true) {
+        accumulator = element;
+        hasNoAccumulator = false;
+      } else {
+        accumulator = iterator(accumulator, element);
+      }
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
